5df679ca0cf4abcb191822f487f58d1dba52901b
Danilo Calderon <Danilo0203@users.noreply.github.com>
2024-01-18 12:54:38 -0600

    02-Desafios JavaScript ONE+Alura

---
+++ 00-iniciandoConJavaScript/02-desafio.js
@@ -0,0 +1,28 @@
+/* Crea un contador que comience en 1 y vaya hasta 10 usando un bucle 'while'. 
+  Muestra cada número. */
+let contador = 1;
+while (contador <= 10) {
+  console.log(contador++);
+}
+
+/* Crea un contador que comience en 10 y vaya hasta 0 usando un bucle 'while'. 
+  Muestra cada número. */
+let contador2 = 10;
+while (contador2 >= 0) {
+  console.log(contador2--);
+}
+
+/* Crea un programa de cuenta regresiva. Pide un número y cuenta desde 0 hasta 
+  ese número utilizando un bucle 'while' en la consola del navegador. */
+let num = prompt("Ingrese un numero");
+let contador3 = 0;
+while (num >= contador3) {
+  console.log(num--);
+}
+/* Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 hasta ese 
+   número utilizando un bucle 'while' en la consola del navegador.
+*/
+let num2 = prompt("Ingrese un numero");
+while (contador3 <= num2) {
+  console.log(contador3++);
+}
